376c03a5eee5abfa6f9e3d00841affa0cf4ce5cc
Javakhir <javakhiryulchibaev7@gmail.com>
2021-06-08 14:56:31 +0500

    changed downloading directory

--- youtube.py
+++ youtube.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 link = input("enter ur youtube url: ")
 yt = YouTube(link)
-videos = yt.streams.res()
+videos = yt.streams.all()
 #это будет передавать все доступные форматы
 video = list(enumerate(videos))
 #пронумерует все доступные форматы начиная с нуля
@@ -12,6 +12,6 @@ print("enter the desired option to download the format")
 dn_option = int(input("enter the option: "))
 #спросит пользователя какой формат видео он хочет загрузить 
 dn_video = videos[dn_option] 
-dn_video.download() #метод для загрузки видео
+dn_video.download('/Download') #метод для загрузки видео
 
 print("download successfully")
