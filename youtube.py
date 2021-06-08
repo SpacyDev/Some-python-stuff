4b85aa151da6f8ebbd0aa41a7390cda6343204e8
Javakhir <javakhiryulchibaev7@gmail.com>
2021-06-08 15:35:25 +0500

    add filter

--- youtube.py
+++ youtube.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 link = input("enter ur youtube url: ")
 yt = YouTube(link)
-videos = yt.streams.all()
+videos = yt.streams.filter(mime_type="video/mp4")
 #это будет передавать все доступные форматы
 video = list(enumerate(videos))
 #пронумерует все доступные форматы начиная с нуля
