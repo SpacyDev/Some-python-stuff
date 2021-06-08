3b76d2f16058fcaa0a42c64f9afe17eaad6ef249
Javakhir <javakhiryulchibaev7@gmail.com>
2021-06-08 14:25:03 +0500

    ready to use

---
+++ youtube.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+link = input("enter ur youtube url: ")
+yt = YouTube(link)
+videos = yt.streams.all()
+#это будет передавать все доступные форматы
+video = list(enumerate(videos))
+#пронумерует все доступные форматы начиная с нуля
+for i in video:
+    print(i)
+    #покажет все доступные форматы пронумерованные
+print("enter the desired option to download the format")
+dn_option = int(input("enter the option: "))
+#спросит пользователя какой формат видео он хочет загрузить 
+dn_video = videos[dn_option] 
+dn_video.download() #метод для загрузки видео
+
+print("download successfully")
